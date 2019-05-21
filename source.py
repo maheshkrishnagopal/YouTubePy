33d2d5ffdcc82657bdc1169aeeb7f3042653cb52
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-05-21 16:49:45 +0530

    Added the functionality of multiprocessing

--- source.py
+++ source.py
@@ -1,6 +1,6 @@
 """
-YouTube Views Booster v.0.1.1
----------------------
+YouTube Views Booster v.1.0.0
+-----------------------------
 
 Python implementation for increasing the YouTube video's views with the help of the video's URL.
 
@@ -16,6 +16,7 @@ NOTE: The program is completely to explore the knowledge and not to monetize or 
 Author: Maheshkrishna A G
 Email: maheshkrishnagopal@gmail.com
 Date: 15-Apr-2019
+Last Modified: 21-May-2019
 """
 
 import time
@@ -24,17 +25,20 @@ import os
 from selenium import webdriver
 from selenium.common.exceptions import *
 from selenium.webdriver.chrome.options import Options
+from multiprocessing import Pool
 
 
-def init_chrome_driver(driver_path):
+def init_chrome_driver(driver_path, yt_url):
     """
-    This function will initiate the Chrome Driver taking the driver path as input, where the chromedriver.exe is 
+    This function will initiate the Chrome Driver taking the driver path as input, where the chromedriver.exe is
     residing. Upon the initiation the driver is automated to navigate to the provided Youtube URL and will play the
     video which will increase the view.
     :param driver_path: directory path where the chromedriver.exe is residing.
-    :return:
+    :param yt_url: YouTube video's URL
+    
+    :return: None
     """
-    chrome_path = 'C:\\Users\\VAIO\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'
+    chrome_path = 'PATH_TO_CHROME.EXE'
     cwd = os.getcwd()
     window_size = "1920,1080"
     chrome_options = Options()
@@ -51,15 +55,26 @@ def init_chrome_driver(driver_path):
     params = {'cmd': 'Page.setDownloadBehavior',
                    'params': {'behavior': 'allow', 'downloadPath': cwd}}
     driver.execute("send_command", params)
-    driver.get('http://www.youtube.com/watch?v=eU8-nu4BHoI')
+    driver.get(yt_url)
     play_button = driver.find_element_by_xpath('//*[@id="movie_player"]/div[4]/button')
     play_button_class = driver.find_elements_by_class_name('ytp-large-play-button.ytp-button')
     play_button.click()
     # play_button_class.click()
-    time.sleep(900)
+    time.sleep(900)                     # Arbitrary time in seconds to watch the video.
     driver.close()
 
 
 if __name__ == "__main__":
-    driver_path = os.getcwd() + '\\chromedriver.exe'
-    init_chrome_driver(driver_path)
+    driver = os.getcwd() + '\\chromedriver.exe'
+    # Get the input for how many number of times the chrome driver has to be initiated.
+    count = int(input())
+    url = input()  # Receive the video's URL as input
+    p = Pool(count)
+    driver_path = []
+    url_passing = []
+    for i in range(1, count+1):
+        url_passing.append(url)
+    for i in range(1, count+1):
+        driver_path.append(driver)
+    print("message_url =", driver_path)
+    output = p.map(init_chrome_driver, zip(driver_path, url_passing))
